d969c1ef09919061b7ddcc6f25a10254b7259c4b
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-01 18:01:12 +0900

    add 404 not found

--- memo.rb
+++ memo.rb
@@ -6,7 +6,7 @@ require 'json'
 require 'securerandom'
 
 get '/memos' do
-  read_memos_table
+  @memos = read_memos_table
 
   erb :index
 end
@@ -16,55 +16,64 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  read_memos_table
+  @memos = read_memos_table
   public_id = SecureRandom.uuid
   @memos[public_id] = { public_id:, title: params[:title], content: params[:content] }
-  write_memos_table
+  write_memos_table(@memos)
 
   redirect "/memos/#{public_id}"
   erb :show
 end
 
 get '/memos/:public_id' do
-  read_memos_table
-  @memo = @memos[params[:public_id].to_sym]
+  @memo = find_memo(params[:public_id])
 
   erb :show
 end
 
 get '/memos/:public_id/edit' do
-  read_memos_table
-  @memo = @memos[params[:public_id].to_sym]
+  @memo = find_memo(params[:public_id])
 
   erb :edit
 end
 
 patch '/memos/:public_id' do
-  read_memos_table
+  @memos = read_memos_table
   @memos[params[:public_id].to_sym] = { public_id: params[:public_id].to_sym, title: params[:title], content: params[:content] }
-  write_memos_table
+  write_memos_table(@memos)
 
   redirect "/memos/#{params[:public_id]}"
   erb :show
 end
 
 delete '/memos/:public_id' do
-  read_memos_table
+  @memos = read_memos_table
   @memos.delete(params[:public_id].to_sym)
-  write_memos_table
+  write_memos_table(@memos)
 
   redirect '/memos'
   erb :index
 end
 
+not_found do
+  '404 Not Found のページです'
+end
+
 def read_memos_table
   File.open('./db/memos.json') do |file|
-    @memos = JSON.parse(file.read, symbolize_names: true)
+    JSON.parse(file.read, symbolize_names: true)
   end
 end
 
-def write_memos_table
+def find_memo(public_id)
+  memos = read_memos_table
+  return not_found if memos[public_id.to_sym].nil?
+
+  memos[public_id.to_sym]
+end
+
+def write_memos_table(memos)
   File.open('./db/memos.json', 'w') do |file|
-    JSON.dump(@memos, file)
+    JSON.dump(memos, file)
   end
 end
