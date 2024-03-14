62181d9d7c79e9b279635d7c71b402ae5fa26cdb
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-14 16:11:27 +0900

    use assert_includes instead of assert

--- test/memo_test.rb
+++ test/memo_test.rb
@@ -32,41 +32,41 @@ class MemoTest < Minitest::Test
   def test_index
     get '/memos'
     assert last_response.status, 200
-    assert last_response.body.include?('買い物リスト')
-    assert last_response.body.include?('スクラム本')
-    assert last_response.body.include?('追加')
+    assert_includes last_response.body, '買い物リスト'
+    assert_includes last_response.body, 'スクラム本'
+    assert_includes last_response.body, '追加'
   end
 
   def test_new
     get '/memos/new', name: 'Frank'
     assert last_response.status, 200
-    assert last_response.body.include?('保存')
+    assert_includes last_response.body, '保存'
   end
 
   def test_show
     get '/memos/b716320e-99d4-4050-bbf7-3c9b26a64665'
     assert last_response.status, 200
-    assert last_response.body.include?('スクラム本')
-    assert last_response.body.include?('SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック')
-    assert last_response.body.include?('変更')
-    assert last_response.body.include?('削除')
+    assert_includes last_response.body, 'スクラム本'
+    assert_includes last_response.body, 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+    assert_includes last_response.body, '変更'
+    assert_includes last_response.body, '削除'
   end
 
   def test_edit
     get '/memos/a90cc4d4-8a66-4fc6-8609-a02f7fe0cf96/edit'
-    assert last_response.body.include?('買い物リスト')
-    assert last_response.body.include?('トマトジュース\nティッシュペーパー')
-    assert last_response.body.include?('変更')
+    assert_includes last_response.body, '買い物リスト'
+    assert_includes last_response.body, 'トマトジュース\nティッシュペーパー'
+    assert_includes last_response.body, '変更'
   end
 
   def test_not_found
     get '/'
-    assert last_response.body.include?('404 Not Found のページです')
+    assert_includes last_response.body, '404 Not Found のページです'
     get '/memos/aaa'
     get '/not_found'
-    assert last_response.body.include?('404 Not Found のページです')
+    assert_includes last_response.body, '404 Not Found のページです'
     get '/memos/aaa/edit'
-    assert last_response.body.include?('404 Not Found のページです')
+    assert_includes last_response.body, '404 Not Found のページです'
   end
 
   def test_create
--- test/xss_test.rb
+++ test/xss_test.rb
@@ -27,23 +27,23 @@ class XssTest < Minitest::Test
   def test_index
     get '/memos'
     assert last_response.status, 200
-    assert last_response.body.include?('&lt;script&gt;alert(&#39;タイトル&#39;)&lt;/script&gt;')
-    assert last_response.body.include?('追加')
+    assert_includes last_response.body, '&lt;script&gt;alert(&#39;タイトル&#39;)&lt;/script&gt;'
+    assert_includes last_response.body, '追加'
   end
 
   def test_show
     get '/memos/c1e3e3e3-8a66-4fc6-8609-a02f7fe0cf96'
     assert last_response.status, 200
-    assert last_response.body.include?('&lt;script&gt;alert(&#39;タイトル&#39;)&lt;/script&gt;')
-    assert last_response.body.include?('&lt;script&gt;alert(&#39;内容&#39;)&lt;/script&gt;')
-    assert last_response.body.include?('変更')
-    assert last_response.body.include?('削除')
+    assert_includes last_response.body, '&lt;script&gt;alert(&#39;タイトル&#39;)&lt;/script&gt;'
+    assert_includes last_response.body, '&lt;script&gt;alert(&#39;内容&#39;)&lt;/script&gt;'
+    assert_includes last_response.body, '変更'
+    assert_includes last_response.body, '削除'
   end
 
   def test_edit
     get '/memos/c1e3e3e3-8a66-4fc6-8609-a02f7fe0cf96/edit'
-    assert last_response.body.include?('<script>alert(\'タイトル\')</script>')
-    assert last_response.body.include?('<script>alert(\'内容\')</script>')
-    assert last_response.body.include?('変更')
+    assert_includes last_response.body, '<script>alert(\'タイトル\')</script>'
+    assert_includes last_response.body, '<script>alert(\'内容\')</script>'
+    assert_includes last_response.body, '変更'
   end
 end
