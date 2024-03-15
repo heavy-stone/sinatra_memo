c6c38cfd8ead9e72da7663165140893604721785
heavy-stone <44592900+hevy-stone@users.noreply.github.com>
2024-03-15 15:47:45 +0900

    use " instead of '

--- test/memo_test.rb
+++ test/memo_test.rb
@@ -18,12 +18,12 @@ class MemoTest < Minitest::Test
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
         'title' => '買い物リスト',
-        'content' => 'トマトジュース\nティッシュペーパー'
+        'content' => "トマトジュース\nティッシュペーパー"
       },
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       }
     }
     write_memos(memos)
@@ -47,7 +47,7 @@ class MemoTest < Minitest::Test
     get '/memos/test-uuid-2'
     assert last_response.status, 200
     assert_includes last_response.body, 'スクラム本'
-    assert_includes last_response.body, 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+    assert_includes last_response.body, 'SCRUM BOOT CAMP<br>アジャイルプラクティスガイドブック'
     assert_includes last_response.body, '変更'
     assert_includes last_response.body, '削除'
   end
@@ -55,7 +55,7 @@ class MemoTest < Minitest::Test
   def test_edit
     get '/memos/test-uuid-1/edit'
     assert_includes last_response.body, '買い物リスト'
-    assert_includes last_response.body, 'トマトジュース\nティッシュペーパー'
+    assert_includes last_response.body, "トマトジュース\nティッシュペーパー"
     assert_includes last_response.body, '変更'
   end
 
@@ -74,24 +74,24 @@ class MemoTest < Minitest::Test
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
         'title' => '買い物リスト',
-        'content' => 'トマトジュース\nティッシュペーパー'
+        'content' => "トマトジュース\nティッシュペーパー"
       },
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       }
     }
     expected_after = {
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
         'title' => '買い物リスト',
-        'content' => 'トマトジュース\nティッシュペーパー'
+        'content' => "トマトジュース\nティッシュペーパー"
       },
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       },
       'new-uuid' => {
         'public_id' => 'new-uuid',
@@ -112,28 +112,28 @@ class MemoTest < Minitest::Test
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
         'title' => '買い物リスト',
-        'content' => 'トマトジュース\nティッシュペーパー'
+        'content' => "トマトジュース\nティッシュペーパー"
       },
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       }
     }
     expected_after = {
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
         'title' => '買い物一覧',
-        'content' => '焼きそばパン\nトマトジュース\nティッシュペーパー'
+        'content' => "焼きそばパン\nトマトジュース\nティッシュペーパー"
       },
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       }
     }
     assert_equal expected_before, read_memos
-    patch '/memos/test-uuid-1', { title: '買い物一覧', content: '焼きそばパン\nトマトジュース\nティッシュペーパー' }
+    patch '/memos/test-uuid-1', { title: '買い物一覧', content: "焼きそばパン\nトマトジュース\nティッシュペーパー" }
     assert_equal expected_after, read_memos
     assert last_response.status, 302
   end
@@ -143,19 +143,19 @@ class MemoTest < Minitest::Test
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
         'title' => '買い物リスト',
-        'content' => 'トマトジュース\nティッシュペーパー'
+        'content' => "トマトジュース\nティッシュペーパー"
       },
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       }
     }
     expected_after = {
       'test-uuid-2' => {
         'public_id' => 'test-uuid-2',
         'title' => 'スクラム本',
-        'content' => 'SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック'
+        'content' => "SCRUM BOOT CAMP\nアジャイルプラクティスガイドブック"
       }
     }
     assert_equal expected_before, read_memos
--- test/xss_test.rb
+++ test/xss_test.rb
@@ -17,8 +17,8 @@ class XssTest < Minitest::Test
     memos = {
       'test-uuid-1' => {
         'public_id' => 'test-uuid-1',
-        'title' => '<script>alert(\'タイトル\')</script>',
-        'content' => '<script>alert(\'内容\')</script>'
+        'title' => '<script>alert("タイトル")</script>',
+        'content' => '<script>alert("内容")</script>'
       }
     }
     write_memos(memos)
@@ -27,23 +27,23 @@ class XssTest < Minitest::Test
   def test_index
     get '/memos'
     assert last_response.status, 200
-    assert_includes last_response.body, '&lt;script&gt;alert(&#39;タイトル&#39;)&lt;/script&gt;'
+    assert_includes last_response.body, '&lt;script&gt;alert(&quot;タイトル&quot;)&lt;/script&gt;'
     assert_includes last_response.body, '追加'
   end
 
   def test_show
     get '/memos/test-uuid-1'
     assert last_response.status, 200
-    assert_includes last_response.body, '&lt;script&gt;alert(&#39;タイトル&#39;)&lt;/script&gt;'
-    assert_includes last_response.body, '&lt;script&gt;alert(&#39;内容&#39;)&lt;/script&gt;'
+    assert_includes last_response.body, '&lt;script&gt;alert(&quot;タイトル&quot;)&lt;/script&gt;'
+    assert_includes last_response.body, '&lt;script&gt;alert(&quot;内容&quot;)&lt;/script&gt;'
     assert_includes last_response.body, '変更'
     assert_includes last_response.body, '削除'
   end
 
   def test_edit
     get '/memos/test-uuid-1/edit'
-    assert_includes last_response.body, '<script>alert(\'タイトル\')</script>'
-    assert_includes last_response.body, '<script>alert(\'内容\')</script>'
+    assert_includes last_response.body, '<script>alert("タイトル")</script>'
+    assert_includes last_response.body, '<script>alert("内容")</script>'
     assert_includes last_response.body, '変更'
   end
 end
